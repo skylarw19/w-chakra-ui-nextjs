f9c490f4ad0005098ef9c00f66a8ec4a4160f180
Skylar Wong <li.q.wong@gmail.com>
2020-10-05 19:20:49 -0500

    change search.js function to async

--- src/pages/api/search.js
+++ src/pages/api/search.js
@@ -1,5 +1,7 @@
-export default (req,res) => {
-    const results = fetch(`http://104.131.65.216:3000/api/search/${req.query.q}`)
+export default async (req,res) => {
+    const results = await fetch(`http://104.131.65.216:3000/api/search/${req.query.q}`)
+    console.log("hello")
+    console.log(results)
     const showsWithServicePlans = results.showsWithServicePlans;  //is results not giving me a json obj?
     let shows = [];
     showsWithServicePlans.forEach(el => {  //cannot read property forEach of undefined. 
