b145d790bb618ae37d0ad28339ab95622d2c61e2
Skylar Wong <li.q.wong@gmail.com>
2020-10-05 19:36:52 -0500

    used .then - data is console logged after hello1. need to make sure data is set to results

--- src/pages/api/search.js
+++ src/pages/api/search.js
@@ -1,7 +1,15 @@
 export default async (req,res) => {
-    const results = await fetch(`http://104.131.65.216:3000/api/search/${req.query.q}`)
+    const results = fetch(`http://104.131.65.216:3000/api/search/${req.query.q}`).then(
+        function(response){
+            response.json().then(function(data) {
+                console.log(data)
+            })
+        }
+    )
     console.log("hello")
-    console.log(results)
+    
+    console.log("hello1")
+    
     const showsWithServicePlans = results.showsWithServicePlans;  //is results not giving me a json obj?
     let shows = [];
     showsWithServicePlans.forEach(el => {  //cannot read property forEach of undefined. 
