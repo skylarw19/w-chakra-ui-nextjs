288444d983340070a31e4d6041aaabc838282168
Skylar Wong <li.q.wong@gmail.com>
2020-10-06 01:57:44 -0500

    /api/search function works and returns an obj

--- src/pages/api/search.js
+++ src/pages/api/search.js
@@ -1,18 +1,10 @@
 export default async (req,res) => {
-    const results = fetch(`http://104.131.65.216:3000/api/search/${req.query.q}`).then(
-        function(response){
-            response.json().then(function(data) {
-                console.log(data)
-            })
-        }
-    )
-    console.log("hello")
-    
-    console.log("hello1")
-    
-    const showsWithServicePlans = results.showsWithServicePlans;  //is results not giving me a json obj?
+    const results = await fetch(`http://104.131.65.216:3000/api/search/${req.query.q}`)
+    const data = await results.json()  //.json() is async
+
     let shows = [];
-    showsWithServicePlans.forEach(el => {  //cannot read property forEach of undefined. 
+
+    data.showsWithServicePlans.forEach(el => {  
         let show = {
             seriesName: el.seriesName,
             overview: el.overview,
