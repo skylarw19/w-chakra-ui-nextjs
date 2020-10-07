4d5f821d44d066a49577cb49097fd6c289c6045e
Skylar Wong <li.q.wong@gmail.com>
2020-10-07 13:09:59 -0500

    change /api/search.js to map function instead of forEach

--- src/pages/api/search.js
+++ src/pages/api/search.js
@@ -1,20 +1,15 @@
 export default async (req,res) => {
     const results = await fetch(`${process.env.apiurl}/${req.query.q}`)
-    const data = await results.json()  //.json() is async
-
-    let shows = [];
-
-    data.showsWithServicePlans.forEach(el => {  
-        let show = {
-            seriesName: el.seriesName,
-            overview: el.overview,
-            image: el.image,
-            network: el.network,
-            servicePlans: el.servicePlans
-        };
-        shows.push(show);
+    const data = await results.json()  
+    const shows = data.showsWithServicePlans.map(el =>{
+            return{    
+                seriesName: el.seriesName,
+                overview: el.overview,
+                image: el.image,
+                network: el.network,
+                servicePlans: el.servicePlans  
+            }
     })
-
     return res.json({shows})
 }
 
