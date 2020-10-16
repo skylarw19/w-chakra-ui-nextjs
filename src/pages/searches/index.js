94ae89934f33f7f70d0eda1c19c697eb0b2b2c0b
Skylar Wong <li.q.wong@gmail.com>
2020-10-15 22:21:54 -0500

    add a SearchHistory page with pages/searches/index.js

--- src/pages/searches/index.js
+++ src/pages/searches/index.js
@@ -1,1 +1,20 @@
 // have full list
+import {
+    Input,
+    Button,
+    Text,
+    Flex,
+  } from '@chakra-ui/core'
+  
+import React, {useState} from 'react'
+import { Container } from '../../components/Container'
+
+const SearchHistoryIndex = () => {
+    return (  
+        <Container>
+            <p> hello past search index pg</p>
+        </Container>
+    );
+}
+
+export default SearchHistoryIndex;
