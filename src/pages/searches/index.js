8951386a07b0d9814df8dd220bf160f20c64bfde
Skylar Wong <li.q.wong@gmail.com>
2020-10-15 23:49:39 -0500

    /api/searchHistory correct gets mongoDB data. /pages/searchHistory runs into error of Only absolute URLs are supported when trying to fetch /api/searchHistory

--- src/pages/searches/index.js
+++ src/pages/searches/index.js
@@ -9,16 +9,29 @@ import {
 import React, {useState} from 'react'
 import { Container } from '../../components/Container'
 import Link from 'next/link'
+import SearchesMade from '../../../models/searchesMade'
 
-const SearchHistoryIndex = () => {
+export default function SearchHistoryIndex(props) {
     return (  
         <Container>
             <p> hello past search index pg</p>
-            <Button>
+            <Button varianColor="yellow"> 
                 <Link href="/"><a>Back to Home</a></Link>
             </Button>
+            {props.searchHistory.map(el =>
+                <p>{el.seriesName}</p>
+                )}
         </Container>
     );
 }
 
-export default SearchHistoryIndex;
+export async function getStaticProps(){
+    //get external data from db
+    const searchHistory = await fetch('/api/searchHistory') 
+    console.log(searchHistory)
+    //the value of the 'props' key will be passed to the SearchHistoryIndex component
+    return {
+        props: searchHistory
+    }
+    
+}
